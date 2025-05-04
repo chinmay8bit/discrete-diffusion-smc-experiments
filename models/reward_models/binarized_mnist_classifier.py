8866c9e8731e85e6b2ae8280ffcbeb9898114b94
Chinmay Pani <chinmay09061998@gmail.com>
2025-05-04 15:14:19 +0100

    Add simple classifier

--- models/reward_models/binarized_mnist_classifier.py
+++ models/reward_models/binarized_mnist_classifier.py
@@ -25,3 +25,16 @@ class BinarizedMNISTClassifier(nn.Module):
         x = F.relu(self.fc1(x))
         x = self.fc2(x)
         return x  # Shape: (B, 10)
+
+
+class SimpleBinarizedMNISTClassifier(nn.Module):
+    def __init__(self):
+        super(SimpleBinarizedMNISTClassifier, self).__init__()
+        self.fc = nn.Linear(28 * 28 * 2, 10)
+
+    def forward(self, x):
+        """
+        x.shape: (B, 1, 28, 28, 2)
+        """
+        x = self.fc(x.flatten(start_dim=1))
+        return x  # Shape: (B, 10)
